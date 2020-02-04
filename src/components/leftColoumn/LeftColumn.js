1e3e5a4ddfc893ae9147ed1f845caa53534471af
Travis Molter <travismolter@gmail.com>
2020-02-03 17:31:13 -0800

    finished writing up resume.json file

--- src/components/leftColoumn/LeftColumn.js
+++ src/components/leftColoumn/LeftColumn.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const LeftColumn = () => {
-    
+const LeftColumn = ({ summary, }) => {
+  <>
+    <div>
+      <h2>Summary</h2>
+      <p>{summary}</p>
+    </div>
+    <div>
+      <h2>Tech Skills</h2>
+    </div>
+  </>;
 };
 
 LeftColumn.propTypes = {
